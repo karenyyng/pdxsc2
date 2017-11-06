a127be2edf785dedfe6003aea83934d5ae3ca6d2
karenyyng <karenyng@ucdavis.edu>
2017-11-05 21:45:05 -0800

    Add message for agreeing to the EULA

--- prep_map.sh
+++ prep_map.sh
@@ -1,5 +1,6 @@
 mkdir -p $HOME/StarCraftII/Maps
 cd ~/StarCraftII/Maps
+echo 'Downloading maps'
 if [[ ! -f Ladder2017Season1.zip ]]; then
   wget http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season1.zip
 fi
@@ -12,6 +13,7 @@ fi
 if [[ ! -f Melee.zip ]]; then
   wget http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip
 fi
+echo 'Agree to the EULA'
 unzip -P iagreetotheeula Ladder2017Season1.zip
 unzip -P iagreetotheeula Ladder2017Season2.zip
 unzip -P iagreetotheeula Ladder2017Season3.zip
