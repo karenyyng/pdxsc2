10ca1e05891ae2dda0b6063524fe3e119a2a4778
karenyyng <karenyng@ucdavis.edu>
2017-11-05 21:34:24 -0800

    Add script to download and unzip maps

---
+++ prep_map.sh
@@ -0,0 +1,20 @@
+if [[ ! -d $HOME/StarCraftII/Maps ]]; then
+  mkdir -p /Users/karenyng/StarCraftII/Maps
+fi
+cd ~/StarCraftII/Maps
+if [[ ! -f Ladder2017Season1.zip ]]; then
+  wget http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season1.zip
+fi
+if [[ ! -f Ladder2017Season2.zip ]]; then
+  wget http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season2.zip
+fi
+if [[ ! -f Ladder2017Season3.zip ]]; then
+  wget http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season3.zip
+fi
+if [[ ! -f Melee.zip ]]; then
+  wget http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip
+fi
+unzip -P iagreetotheeula Ladder2017Season1.zip
+unzip -P iagreetotheeula Ladder2017Season2.zip
+unzip -P iagreetotheeula Ladder2017Season3.zip
+unzip -P iagreetotheeula Melee.zip
